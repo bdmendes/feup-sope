3e806c5dc4620a939e7db65f4b8c5dc692eac529
Bruno Mendes <brod56@sapo.pt>
2021-02-26 16:02:36 +0000

    Add ex 8

--- processes_threads/ex8/signal.c
+++ processes_threads/ex8/signal.c
@@ -1,8 +1,11 @@
 #include <signal.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-void func(int signo) { fprintf(stderr, "\nReceived signal %d!\n", signo); }
+void func(int signo) {
+  fprintf(stderr, "\nDefault handler, received signal %d!\n", signo);
+}
 
 int main() {
   struct sigaction new, old;
@@ -16,13 +19,25 @@ int main() {
   new.sa_mask = smask;
   new.sa_flags = 0; // usually works
 
-  if (sigaction(SIGUSR1, &new, &old) == -1)
+  if (sigaction(SIGUSR1, &new, &old) ==
+      -1) // better alternative to signal(), which is an older version with less
+          // options; allows setting up default signal handling action
     perror("sigaction");
 
-  printf("\nSIGUSR1 handler is ready...");
-  printf("\n\t Try: \"sh> kill -SIGUSR1 %d\".\n", getpid());
-
-  pause(); // wait
+  pid_t pid = fork();
+  switch (pid) {
+  case -1:
+    perror("fork failed");
+    return -1;
+  case 0: // Child
+    printf("Hello ");
+    kill(pid, SIGUSR1);
+    break;
+  default: // Parent
+    pause();
+    printf("World!\n");
+    break;
+  }
 
   return 0;
 }
